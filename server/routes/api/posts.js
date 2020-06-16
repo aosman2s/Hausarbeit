21fb01c8ad375f83a2bfbb5f367fb1ef425b5d6f
ahmed <ahmed.osama22@live.com>
2020-06-16 11:09:40 +0200

    Edit API

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongodb = require('mongodb');
 const fs = require('fs');
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
@@ -19,17 +18,17 @@ router.get('/html/img', async (req, res )=> {
 
 router.get('/html/headings', async (req, res )=> {
     var txt= await loadFile('./server/files/headings.txt');
-    res.send(txt);
+    res.send({title: 'HTML Heading', content : txt});
 });
 
 router.get('/html/paragraphs', async (req, res )=> {
     var txt= await loadFile('./server/files/paragraphs.txt');
-    res.send(txt);
+    res.send({title: 'HTML Paragraphs', content : txt});
 });
 
 router.get('/html/tables', async (req, res )=> {
     var txt= await loadFile('./server/files/tables.txt');
-    res.send(txt);
+    res.send({title: 'HTML Tables', content : txt});
 });
 
 /*
@@ -38,24 +37,24 @@ router.get('/html/tables', async (req, res )=> {
 
 router.get('/css', async (req, res )=> {
     var txt= await loadFile('./server/files/css.txt');
-    res.send(txt);
+    res.send({title: 'CSS', content : txt});
 });
 router.get('/css/img', async (req, res )=> {
     res.sendFile(path.join(appDir, 'imgs', 'CSS.png'));
 });
 router.get('/css/selector', async (req, res )=> {
    var txt= await loadFile('./server/files/selector.txt');
-    res.send(txt);
+    res.send({title: 'CSS Selector', content : txt});
 });
 
 router.get('/css/colors', async (req, res )=> {
     var txt= await loadFile('./server/files/colors.txt');
-    res.send(txt);
+    res.send({title: 'CSS Colors', content : txt});
 });
 
 router.get('/css/boxes', async (req, res )=> {
     var txt= await loadFile('./server/files/boxes.txt');
-    res.send(txt);
+    res.send({title: 'CSS Boxes', content : txt});
 });
 
 /*
@@ -64,7 +63,7 @@ router.get('/css/boxes', async (req, res )=> {
 
 router.get('/javaScript', async (req, res )=> {
     var txt= await loadFile('./server/files/javaScript.txt');
-    res.send(txt);
+    res.send({title: 'JavaScript', content : txt});
 });
 router.get('/javaScript/img', async (req, res )=> {
     res.sendFile(path.join(appDir, 'imgs', 'JavaScript.png'));
@@ -72,12 +71,12 @@ router.get('/javaScript/img', async (req, res )=> {
 
 router.get('/javaScript/functions', async (req, res )=> {
     var txt= await loadFile('./server/files/functions.txt');
-    res.send(txt);
+    res.send({title: 'JavaScript Functions', content : txt});
 });
 
 router.get('/javaScript/objects', async (req, res )=> {
     var txt= await loadFile('./server/files/objects.txt');
-    res.send(txt);
+    res.send({title: 'JavaScript Objects', content : txt});
 });
 
 
